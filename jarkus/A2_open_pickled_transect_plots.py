16a321279bc2b26d4b98324fd831686234b8e165
christa-tudelft <christa.vanijzendoorn@gmail.com>
2020-06-08 11:16:03 +0200

    Reopen pickled transect plot. As example dune foots are plotted in the reopened figures.

--- jarkus/A2_open_pickled_transect_plots.py
+++ jarkus/A2_open_pickled_transect_plots.py
@@ -6,19 +6,50 @@ Created on Thu Oct 17 15:43:48 2019
 """
 
 ##################################
+####          PACKAGES        ####
+##################################
+from visualisation import reopen_pickle
+from IPython import get_ipython
+# Execute %matplotlib auto first, otherwise you get an error
+get_ipython().run_line_magic('matplotlib', 'auto')
+import json
+import pickle
+import matplotlib.pyplot as plt
+
+##################################
 ####    USER-DEFINED REQUEST  ####
 ##################################
-# Set the transect and years for retrieval request
-transect_name = "06_Texel"
-transect_req = 6002521
-years_requested = range(1970, 2020, 1)
+# load basic settings from .txt file
+with open("C:/Users/cijzendoornvan/Documents/GitHub/jarkus/jarkus/Settings.txt") as file:
+    settings = json.load(file)
+
+# Set the transect and years for retrieval request    
+transect_name = "08_Meijendel"
+transect_req = [8009575]
+years_requested = range(2000, 2020, 1)
+
+##################################
+####  LOAD DIMENSIONS FILE    ####
+##################################
+Dir_pickles = settings['Dir_C3']
+trsct = str(transect_req[0])
+
+pickle_file = Dir_pickles + 'Transect_' + trsct + '_dataframe.pickle'
+Dimensions = pickle.load(open(pickle_file, 'rb'))
 
 ##################################
 ####  REOPEN PICKLED FIGURE   ####
 ##################################
-# Execute %matplotlib auto first, otherwise you get an error
-from visualisation import reopen_pickle
 fig_transect = str(transect_req[0])
-Dir_fig = "C:\\Users\\cijzendoornvan\\Documents\\GitHub\\jarkus\\jarkus\\Figures\\" + transect_name + "\\"
+Dir_fig = settings['Dir_A']
 
-reopen_pickle(Dir_fig, fig_transect)
+# Load figure from disk and display
+fig = pickle.load(open(Dir_fig + 'Transect_' + fig_transect + '.fig.pickle','rb'))
+
+plt.plot(Dimensions['DF_pybeach_mix_x'], Dimensions['DF_pybeach_mix_y'], 'g^')
+plt.plot(Dimensions['DF_der_x'], Dimensions['DF_der_y'], 'bs')
+plt.plot(Dimensions['DF_fix_x'], Dimensions['DF_fix_y'], 'ro')
+
+    
+plt.show()
+
---
+++ jarkus/A3_create_transect_plots_with_dune_foots.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 09:19:29 2020
+
+@author: cijzendoornvan
+"""
+##################################
+####          PACKAGES        ####
+##################################
+import json
+from jarkus.transects import Transects
+from visualisation import multilineplot
+import numpy as np
+from scipy.interpolate import griddata
+from IPython import get_ipython
+import matplotlib.pyplot as plt
+import pickle
+get_ipython().run_line_magic('matplotlib', 'auto')
+
+##################################
+####       RETRIEVE DATA      ####
+##################################
+# load basic settings from .txt file
+with open("C:/Users/cijzendoornvan/Documents/GitHub/jarkus/jarkus/Settings.txt") as file:
+    settings = json.load(file)
+
+# Collect the JARKUS data from the server
+Jk = Transects(url= settings['url'])
+ids = Jk.get_data('id')
+
+##################################
+####    USER-DEFINED REQUEST  ####
+##################################
+# Set whether all transect should be analysed or define a retrieval request
+execute_all_transects = False
+
+# Set which years should be analysed
+years_requested = list(range(1970, 2005, 5))
+
+if execute_all_transects == False:
+    # Set the transect and years for retrieval request
+    transect_name   = "08_Meijendel"
+    #transect_req = np.arange(8009325, 8009750, 1)
+    #transect_req        = [8007975]
+    #transect_req        = [8007400]
+    #transect_req        = [8007250]
+    transect_req        = [9011535] #Example transect!
+    #transect_req        = [16001775]
+    idxs = np.isin(transect_req, ids) # check which transect are available of those that were requested
+    ids_filtered = np.array(transect_req)[np.nonzero(idxs)[0]]
+    Dir_plots = settings['Dir_figures'] + transect_name.replace(" ","") + "/"
+else:
+    # Filter out location that are not suitable for analysis. Based on Kustlijnkaarten 2019, RWS.
+    with open("C:/Users/cijzendoornvan/Documents/GitHub/jarkus/jarkus/Filter.txt") as file:
+        filter_transects = json.load(file)
+    
+    filter_all = []
+    for key in filter_transects:
+        filter_all += list(range(int(filter_transects[key]["begin"]), int(filter_transects[key]["eind"])))
+        
+    # Use this if you want to skip transects, e.g. if your battery died during running...
+    #skip_transects = list(ids[1:1710])
+    #filter_all = filter_all + skip_transects
+        
+    ids_filtered = [x for x in ids if x not in filter_all]
+    Dir_plots = settings['Dir_A3']
+    #%%
+##################################
+####   CREATE AND SAVE PLOTS  ####
+##################################
+# Set x and y limit of plots - Leave lists empty (i.e. []) for automatic axis limits
+xlimit = [-100,150] # EXAMPLE: [-400,1000]
+ylimit = [-1, 16] # EXAMPLE: [-10,22]
+
+years_requested_str = [str(yr) for yr in years_requested]
+
+# Interpolate x and y along standardized cross shore axis
+cross_shore = list(range(-3000, 9320, 1))
+
+# Here the JARKUS filter is set and the data for each requested id and year are retrieved
+for idx in ids_filtered:
+    print(idx)
+    trsct = str(idx)
+
+    Jk = Transects(url= settings['url'])
+    df, years_available = Jk.get_dataframe(idx, years_requested)
+    
+    # Convert elevation data for each year of the transect into array that can easily be plotted
+    y_all = [] 
+    for i, yr in enumerate(years_requested_str):
+        if yr in years_available:
+            y = np.array(df.loc[trsct, yr]['y'])
+            x = np.array(df.loc[trsct, yr]['x'])
+            y_grid = griddata(x,y,cross_shore)
+            if i == 0:
+                y_all = y_grid
+            else:
+                y_all = np.column_stack((y_all, y_grid))
+                y_grid = griddata(x,y,cross_shore)
+        else:
+            y_grid = np.empty((len(cross_shore),))
+            y_grid[:] = np.nan
+            if i == 0:
+                y_all = y_grid
+            else:
+                y_all = np.column_stack((y_all, y_grid))
+    
+    # Here for each transect a multilineplot is made and saved in the predefined directory
+    #title =  "Transect {}".format(str(idx))
+    #title =  "Transect near Hoek van Holland (before large nourishment)"
+    title = ""
+    fig = multilineplot(cross_shore, y_all, years_requested, title, "Cross shore distance [m]", "Elevation [m to datum]", xlimit, ylimit)
+    
+    ##################################
+    ####  LOAD DIMENSIONS FILE    ####
+    ##################################
+    Dir_pickles = settings['Dir_C3']
+
+    pickle_file = Dir_pickles + 'Transect_' + trsct + '_dataframe.pickle'
+    Dimensions = pickle.load(open(pickle_file, 'rb'))
+
+    plt.plot(Dimensions['DF_fix_x'][years_requested_str], Dimensions['DF_fix_y'][years_requested_str], 'kx', markersize=12, mew=3, label = 'Historic')
+    plt.plot(Dimensions['DF_der_x'][years_requested_str], Dimensions['DF_der_y'][years_requested_str], 'rx', markersize=12, mew=3, label = 'Derivative')
+    plt.plot(Dimensions['DF_pybeach_mix_x'][years_requested_str], Dimensions['DF_pybeach_mix_y'][years_requested_str], 'gx', markersize=12, mew=3, label = 'Machine Learning')
+    
+    plt.legend(loc='upper right', fontsize = 24)
+    
+    
+    # Show the figure    
+    plt.show()
+    
+    # Save figure as png in predefined directory
+    #plt.savefig(Dir_plots + 'Transect_' + title[9:] + '.png')
+    #pickle.dump(fig, open(Dir_plots + 'Transect_' + title[9:] + '.fig.pickle', 'wb'))
+    
+    #plt.close()
+
+"""
+# Dummy data for plotting
+# years = [1965, 1966, 1967]
+# cross_shore = [np.array([-50, 0, 50, 100, 150]), np.array([-50, 0, 50, 100, 150]), np.array([-50, 0, 50, 100, 150])]
+# elevation = [np.array([-5, 0, 4, 9, 15]), np.array([-3, 0, 3, 7, 12]), np.array([-4, 0, 4, 8, 14])]
+""" 
